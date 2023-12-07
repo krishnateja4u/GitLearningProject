92e0cac7d30ee2b136263a982f2049c5d1a2b69b
krishnateja4u <krishnateja.akella@gmail.com>
2023-12-07 12:38:35 +0530

    new universal changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
         public static void main(String[] args) {
             System.out.println("Hello world!");
 
-            System.out.println("Hello universe2");
+            System.out.println("Hello universe3");
 
         }
 
